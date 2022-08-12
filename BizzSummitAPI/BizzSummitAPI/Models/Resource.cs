ee901c050b3c6fe716f83090e651984c9a95d80b
jsegura1@kpmg.com <jsegura1@kpmg.com>
2022-08-12 12:12:42 +0200

    Adapted resource model

--- BizzSummitAPI/BizzSummitAPI/Models/Resource.cs
+++ BizzSummitAPI/BizzSummitAPI/Models/Resource.cs
@@ -8,16 +8,10 @@ namespace BizzSummitAPI
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
-        [JsonProperty(PropertyName = "firstName")]
-        public string FirstName { get; set; }
+        [JsonProperty(PropertyName = "fullName")]
+        public string FullName { get; set; }
 
-        [JsonProperty(PropertyName = "lastName")]
-        public string LastName { get; set; }
-
-        [JsonProperty(PropertyName = "email")]
-        public string Email { get; set; }
-
-        [JsonProperty(PropertyName = "phone")]
-        public string Phone { get; set; }
+       [JsonProperty(PropertyName = "mail")]
+        public string Mail { get; set; }
     }
 }
